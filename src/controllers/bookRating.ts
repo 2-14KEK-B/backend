42e608939ea8e901182aa83e825a22b04a891c02
mecsko <mecserdavid@gmail.com>
2022-11-21 09:03:00 +0100

    update find book by rated user logic

--- src/controllers/bookRating.ts
+++ src/controllers/bookRating.ts
@@ -1,17 +1,17 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
 import validationMiddleware from "@middlewares/validation";
-import HttpError from "@exceptions/Http";
-import type { BookRating, CreateBookRating } from "@interfaces/bookRating";
-import type Controller from "@interfaces/controller";
+import authenticationMiddleware from "@middlewares/authentication";
+import authorizationMiddleware from "@middlewares/authorization";
 import bookModel from "@models/book";
 import userModel from "@models/user";
 import isIdValid from "@utils/idChecker";
-import { Router, Request, Response, NextFunction } from "express";
-import { BookRatingDto } from "@validators/book";
-import authenticationMiddleware from "@middlewares/authentication";
-import { Types } from "mongoose";
-import authorizationMiddleware from "@middlewares/authorization";
 import StatusCode from "@utils/statusCodes";
+import { BookRatingDto } from "@validators/book";
+import HttpError from "@exceptions/Http";
 import type { Book } from "@interfaces/book";
+import type { BookRating, CreateBookRating } from "@interfaces/bookRating";
+import type Controller from "@interfaces/controller";
 
 export default class BookRatingController implements Controller {
     path = "/";
@@ -34,9 +34,10 @@ export default class BookRatingController implements Controller {
             const bookId = req.params["id"];
             if (!(await isIdValid(this.book, [bookId], next))) return;
 
-            const bookRatings = await this.book.findById(bookId, { ratings: 1 }).lean<BookRating[]>().exec();
+            const bookRatings = await this.book.findById(bookId, { _id: 0, ratings: 1 }).lean<{ ratings: BookRating[] }>().exec();
             if (!bookRatings) return next(new HttpError("Failed to get rating of the book"));
-            res.json(bookRatings);
+
+            res.json(bookRatings.ratings);
         } catch (error) {
             next(new HttpError(error));
         }
@@ -48,10 +49,8 @@ export default class BookRatingController implements Controller {
             const bookId = req.params["id"];
             if (!(await isIdValid(this.book, [bookId], next))) return;
 
-            const book = await this.book.findById(bookId).exec();
-
-            const ratedAlready = book?.ratings?.find(rate => isUsersRate(rate, userId));
-            if (ratedAlready) return next(new HttpError("Already rated this book."));
+            const book = await this.book.findOne({ _id: bookId, "ratings.from_id": userId }).lean<Book>().exec();
+            if (book) return next(new HttpError("Already rated this book."));
 
             const rateData: CreateBookRating = req.body;
             const ratedBook = await this.book
@@ -60,7 +59,7 @@ export default class BookRatingController implements Controller {
                 .exec();
             if (!ratedBook) return next(new HttpError("Failed to rate book"));
 
-            const updatedUser = await this.user.findByIdAndUpdate(userId, { $push: { rated_books: book?._id } }, { returnDocument: "after" });
+            const updatedUser = await this.user.findByIdAndUpdate(userId, { $push: { rated_books: ratedBook?._id } }, { returnDocument: "after" });
             if (!updatedUser) return next(new HttpError("Failed to update the user"));
 
             res.json(ratedBook);
@@ -75,10 +74,8 @@ export default class BookRatingController implements Controller {
             const bookId = req.params["id"];
             if (!(await isIdValid(this.book, [bookId], next))) return;
 
-            const book = await this.book.findById(bookId).exec();
-
-            const rated = book?.ratings?.find(rate => isUsersRate(rate, userId));
-            if (!rated) return next(new HttpError("You did not rate this book."));
+            const book = await this.book.findOne({ _id: bookId, "ratings.from_id": userId }).lean<Book>().exec();
+            if (!book) return next(new HttpError("You did not rate this book."));
 
             const { acknowledged } = await this.book.updateOne({ _id: bookId }, { $pull: { ratings: { from_id: new Types.ObjectId(userId) } } });
 
@@ -92,8 +89,4 @@ export default class BookRatingController implements Controller {
             next(new HttpError(error));
         }
     };
-}
-
-function isUsersRate(rate: BookRating, userId: string) {
-    return rate.from_id.valueOf() === userId;
 }
