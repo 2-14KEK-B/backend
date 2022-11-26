00bd655896a85dc0156f5bfa3547143bdcbee462
mecsko <mecserdavid@gmail.com>
2022-11-26 14:42:44 +0100

    add drop db after each test suit

--- config/setupFilesAfterEnv.ts
+++ config/setupFilesAfterEnv.ts
@@ -1,14 +1,13 @@
-import { disconnect } from "mongoose";
+import { disconnect, connection } from "mongoose";
 import connectToDatabase from "@db/connectToDatabase";
 import { closeSessionStore } from "@db/sessionStore";
 
 beforeAll(async () => {
-    // put your client connection code here, example with mongoose:
     await connectToDatabase(process.env["TEST_URI"]);
 });
 
 afterAll(async () => {
-    // put your client disconnection code here, example with mongodb:
+    await connection.db.dropDatabase();
     await closeSessionStore();
     await disconnect();
 });
