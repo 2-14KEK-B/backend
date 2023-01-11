49334f35599e34f436ef67d903f58c4a55f0e40b
mecsko <mecserdavid@gmail.com>
2023-01-11 13:19:32 +0100

    put socket into a separate file

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express, { Application, json, Request, Response, urlencoded } from "express";
-import { Server as Socket } from "socket.io";
 import { createServer, Server } from "http";
 import session, { SessionOptions } from "express-session";
 import cors from "cors";
 import morgan from "morgan";
+import { initSocket } from "./socket";
 import { createSessionStore } from "@db/sessionStore";
 import errorMiddleware from "@middlewares/error";
 import env from "@config/validateEnv";
 import corsOptions from "@config/corsOptions";
 import StatusCode from "@utils/statusCodes";
 import type Controller from "@interfaces/controller";
-import type { Message, MessageContent } from "@interfaces/message";
 
 export default class App {
     public app: Application;
@@ -84,56 +83,7 @@ export default class App {
     }
 
     public initSocketIO() {
-        const io = new Socket(this.server, { cors: corsOptions });
+        const io = initSocket(this.server);
         this.app.io = io;
-
-        let onlineUsers: { user_id: string; socket_id: string }[] = [];
-
-        io.on("connection", socket => {
-            socket.on("disconnecting", () => {
-                const disconnectedId = socket.id;
-                // console.log(
-                //     disconnectedId,
-                //     onlineUsers.find(u => u.socket_id == disconnectedId),
-                // );
-                onlineUsers = onlineUsers.filter(user => user.socket_id !== disconnectedId);
-
-                // onlineUsers.forEach(user => {
-                //     socket.to(user.socket_id).emit(
-                //         "other-users",
-                //         onlineUsers.map(user => user.user_id),
-                //     );
-                // });
-            });
-            socket.on("user-online", (userId: string) => {
-                // socket.emit(
-                //     "other-users",
-                //     onlineUsers.map(user => user.user_id),
-                // );
-
-                if (!onlineUsers.some(u => u.user_id == userId)) {
-                    onlineUsers.push({ user_id: userId, socket_id: socket.id });
-                }
-                // console.log(onlineUsers);
-                // socket.broadcast.emit("new-user", userId);
-            });
-
-            socket.on("send-msg-cnt", (data: { to: string; message: MessageContent }) => {
-                // console.log("send-msg-cnt to: ", { to: data.to });
-                const sendUserSocket = onlineUsers.find(user => user.user_id === data.to);
-                if (sendUserSocket) {
-                    socket.to(sendUserSocket.socket_id).emit("recieve-msg-cnt", data.message);
-                    socket.to(sendUserSocket.socket_id).emit("msg-sent");
-                }
-            });
-
-            socket.on("send-new-msg", (data: { to: string; message: Message }) => {
-                // console.log("send-new-msg to: ", { to: data.to });
-                const sendUserSocket = onlineUsers.find(user => user.user_id === data.to);
-                if (sendUserSocket) {
-                    socket.to(sendUserSocket.socket_id).emit("recieve-new-msg", data.message);
-                }
-            });
-        });
     }
 }
---
+++ src/socket.ts
@@ -0,0 +1,71 @@
+import { Server } from "socket.io";
+import corsOptions from "@config/corsOptions";
+import type { Message, MessageContent } from "@interfaces/message";
+import type { Server as HttpServer } from "http";
+
+interface ServerToClientEvents {
+    "recieve-msg-cnt": (message: MessageContent) => void;
+    "msg-sent": () => void;
+    "recieve-new-msg": (message: Message) => void;
+}
+
+interface ClientToServerEvents {
+    "user-online": (userId: string) => void;
+    "send-msg-cnt": (data: { to: string; message: MessageContent }) => void;
+    "send-new-msg": (data: { to: string; message: Message }) => void;
+}
+
+export function initSocket(server: HttpServer) {
+    const io = new Server<ClientToServerEvents, ServerToClientEvents>(server, {
+        cors: corsOptions,
+    });
+    let onlineUsers: { user_id: string; socket_id: string }[] = [];
+
+    io.on("connection", socket => {
+        socket.on("disconnecting", () => {
+            const disconnectedId = socket.id;
+            // console.log(
+            //     disconnectedId,
+            //     onlineUsers.find(u => u.socket_id == disconnectedId),
+            // );
+            onlineUsers = onlineUsers.filter(user => user.socket_id !== disconnectedId);
+
+            // onlineUsers.forEach(user => {
+            //     socket.to(user.socket_id).emit(
+            //         "other-users",
+            //         onlineUsers.map(user => user.user_id),
+            //     );
+            // });
+        });
+        socket.on("user-online", (userId: string) => {
+            // socket.emit(
+            //     "other-users",
+            //     onlineUsers.map(user => user.user_id),
+            // );
+
+            if (!onlineUsers.some(u => u.user_id == userId)) {
+                onlineUsers.push({ user_id: userId, socket_id: socket.id });
+            }
+            // console.log(onlineUsers);
+            // socket.broadcast.emit("new-user", userId);
+        });
+
+        socket.on("send-msg-cnt", (data: { to: string; message: MessageContent }) => {
+            // console.log("send-msg-cnt to: ", { to: data.to });
+            const sendUserSocket = onlineUsers.find(user => user.user_id === data.to);
+            if (sendUserSocket) {
+                socket.to(sendUserSocket.socket_id).emit("recieve-msg-cnt", data.message);
+                socket.to(sendUserSocket.socket_id).emit("msg-sent");
+            }
+        });
+
+        socket.on("send-new-msg", (data: { to: string; message: Message }) => {
+            // console.log("send-new-msg to: ", { to: data.to });
+            const sendUserSocket = onlineUsers.find(user => user.user_id === data.to);
+            if (sendUserSocket) {
+                socket.to(sendUserSocket.socket_id).emit("recieve-new-msg", data.message);
+            }
+        });
+    });
+    return io;
+}
