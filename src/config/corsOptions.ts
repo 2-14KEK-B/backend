673592fed88c94832887f2ccae324b1aa88300cd
mecseridavid <mecserdavid@gmail.com>
2023-02-02 14:34:52 +0100

    working facebook login

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,6 +1,12 @@
 import type cors from "cors";
 
 export default <cors.CorsOptions>{
-    origin: ["http://localhost:4000", "http://127.0.0.1:4000", "https://bookswap.onrender.com"],
+    origin: [
+        "http://localhost:4000",
+        "https://localhost:4000",
+        "http://127.0.0.1:4000",
+        "https://127.0.0.1:4000",
+        "https://bookswap.onrender.com",
+    ],
     credentials: true,
 };
